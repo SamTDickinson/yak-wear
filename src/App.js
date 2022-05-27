514d88b74735e28cffe3d26e604f9b47b14768b8
Sam Dickinson <sam.t.dickinson@gmail.com>
2022-05-27 10:12:42 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { Route, Routes, Outlet} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation.component";
-
+import Navigation from "./routes/navigation/navigation.component";
 
 
 const Shop = () => {
@@ -10,7 +9,6 @@ const Shop = () => {
             <div>
                 <h1> I am the Shop</h1>
             </div>
-            <Outlet />
         </div>
     )
 }
@@ -18,9 +16,9 @@ const Shop = () => {
 const App = () => {
     return (
         <Routes>
-            <Route path='/' element={ <Navigation />}>
-                <Route index element={ <Home />}/>
-                <Route path={'/shop'} element={ <Shop />} />
+            <Route path='/' element={<Navigation/>}>
+                <Route index element={<Home/>}/>
+                <Route path={'/shop'} element={<Shop/>}/>
             </Route>
         </Routes>
     );
