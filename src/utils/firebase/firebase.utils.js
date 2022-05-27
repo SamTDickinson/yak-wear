9c611cc73a6f699c81498bed8ccedff4d91f591b
Sam Dickinson <sam.t.dickinson@gmail.com>
2022-05-27 16:14:52 +0100

    Added sign in component and firebase utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,10 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
-import  { ReactComponent as APIKeys} from "../../secrets/api-keys";
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
-'../../secrets/api-keys'
-
-const firebaseConfig = APIKeys;
+const firebaseConfig = {
+    apiKey: "AIzaSyC5pTUHiymluY3yAc31kGAGRt3hhUA9ILg",
+    authDomain: "yak-wear-db.firebaseapp.com",
+    projectId: "yak-wear-db",
+    storageBucket: "yak-wear-db.appspot.com",
+    messagingSenderId: "509585521406",
+    appId: "1:509585521406:web:f004b35aacd503261483af",
+    measurementId: "G-YVE8QGX6HE"
+};
 
 // Initialize Firebase
 const firebasaeApp = initializeApp(firebaseConfig);
@@ -16,3 +22,25 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+    return userDocRef;
+}
