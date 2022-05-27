a6f1f1695301164619322dbdd4aba99177f0ff50
Sam Dickinson <sam.t.dickinson@gmail.com>
2022-05-27 14:45:42 +0100

    setting up google auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,8 +1,18 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import  { ReactComponent as APIKeys} from "../../secrets/api-keys";
 
-const firebaseConfig = {
+'../../secrets/api-keys'
 
-};
+const firebaseConfig = APIKeys;
 
 // Initialize Firebase
 const firebasaeApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
